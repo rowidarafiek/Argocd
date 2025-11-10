634959913132be7694365f3faef5c0618a9be591
rowidarafiek <rowidarafiek83@gmail.com>
2025-11-10 14:21:53 +0200

    update deploy function

--- vars/deployOnK8s.groovy
+++ vars/deployOnK8s.groovy
@@ -1,6 +1,7 @@
 def call(String imageRepo, String imageTag, String namespace) {
     def image = "${imageRepo}:${imageTag}"
-    sh "kubectl apply -n ${namespace} -f k8s/deployment.yaml"
+    sh "kubectl apply -n ${namespace} -f deployment.yaml"
     sh "kubectl set image deployment/my-app my-app=${image} -n ${namespace}"
 }
+
 
